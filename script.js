e88533718721e22d9cc2a4a9ebdaeed1bf663b56
rasmus petersen <rasm00f1@stud.kea.dk>
2021-02-10 13:48:43 +0100

    jobs done

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const arr = [];
+let counter = 0;
+
+console.log(arr);
+
+loop();
+
+function loop() {
+  console.clear();
+  // run loop every 1 second, add one to counter
+  setTimeout(loop, 1000);
+  counter++;
+  if (arr.length != 9) {
+    arr.unshift(counter);
+  } else {
+    arr.unshift(counter);
+    arr.pop();
+  }
+  console.log(arr);
+}
