5950d8657c389a8834a4e75303cc829e436f6129
rasmus petersen <rasm00f1@stud.kea.dk>
2021-02-11 11:24:14 +0100

    new script

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function loop() {
   setTimeout(loop, 1000);
   counter++;
   if (arr.length != 9) {
+    // adds new element to the begging of the array
     arr.unshift(counter);
   } else {
     arr.unshift(counter);
+    // deletes element from the end of the array
     arr.pop();
   }
   console.log(arr);
